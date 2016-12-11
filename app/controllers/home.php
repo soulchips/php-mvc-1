0a496322de1e1ab9aa657b9ae930933db168e642
AkilAikman <akil.aikman@ultreyalogistics.com>
2016-12-11 03:04:25 -0500

    first commit, huzzahgit add .

---
+++ app/controllers/home.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* 
+*/
+class Home extends Controller
+{
+	public function index($name = '')
+	{
+		$this->view('home/index', ['name'=>$user->name]);
+	}
+
+	public function create($username='', $email='')
+	{
+		User::create([
+			'username'=>$username,
+			'email'=>$email
+		]);
+	}
+}
